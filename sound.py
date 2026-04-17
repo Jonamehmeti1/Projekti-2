eaaaeec22bb96445f89ba20feee3fb40f46fdbd1
Agnesa145 <agnesa.godeni@student.uni-pr.edu>
2026-04-17 21:56:10 +0200

    Update sound.py

--- sound.py
+++ sound.py
@@ -5,3 +5,14 @@ def play_morse(morse):
     frequency = 800
     dot_duration = 150
     dash_duration = 450
+
+    for symbol in morse:
+        if symbol == '.':
+            winsound.Beep(frequency, dot_duration)
+        elif symbol == '-':
+            winsound.Beep(frequency, dash_duration)
+        elif symbol == ' ':
+            time.sleep(0.2)
+        elif symbol == '/':
+            time.sleep(0.6)
+
